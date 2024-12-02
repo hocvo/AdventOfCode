4a96c4fcfaa5e7884c317a0ff2781e4ae2ac8ef4
Hoc Vo <chohocvo@gmail.com>
2024-12-01 22:33:21 -0800

    Add Day 2 and templates for other days

---
+++ 2024/d12.py
@@ -0,0 +1,21 @@
+import util
+lines = util.parse('test.txt')
+# convert to int: int(str)
+# convert to str: str(any)
+# list(): [any,any,...]
+# set(): {any,any,...}
+# frozenset(): immutable set
+# dict(): {k:v, k:v, ...}
+# tuple():  An ordered, immutable collection of items (e.g., (1, 2, "apple"))
+# range: Represents a sequence of numbers (e.g., range(5), range(1, 5), range(1,5,2))
+
+def part1():
+    count = 0
+    print(count)
+
+#part 2
+def part2():
+    print(count)
+
+part1()
+part2()
