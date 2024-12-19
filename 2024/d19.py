bf3455e8954b7d021800baa2ffb269f14e4e1c49
Hoc Vo <chohocvo@gmail.com>
2024-12-18 23:31:04 -0800

    d19p2 in work

--- 2024/d19.py
+++ 2024/d19.py
@@ -13,7 +13,7 @@ np.seterr(divide='ignore', invalid='ignore')
 np.set_printoptions(linewidth=100, formatter={'all': lambda x: f'{x:<1}'})
 
 sys.setrecursionlimit(99999)
-lines = util.parse('d19.txt')
+lines = util.parse('test.txt')
 #lines = util.parse('test.txt')
 # convert to int: int(str)
 # convert to str: str(any)
@@ -30,24 +30,33 @@ def main():
     countPos = 0
     # print(avail)
     for pattern in param[1]:
-        pos = recurse(avail, pattern)
-        if pos:
-            countPos += 1
+        # pos += recurse(avail, pattern)
+        i = len(pattern)
+        print("Processing", pattern)
+        arrange = []
+        while i > 0:
+            isPos = recurse(avail, pattern[:i]) and recurse(avail, pattern[i:], arrange)
+            if isPos:
+                print("Found one", pattern[:i], pattern[i:], countPos+1)
+                countPos += 1
+            i -= 1
     print(countPos)
-def recurse(avail, pattern):
+def recurse(avail, pattern, arrange):
     # print("Checking", pattern)
+    if pattern == '':
+        return True
     possible = False
     if pattern in seen:
         # print("Found in seen")
         return seen[pattern]
     if pattern in avail:
         # print("Found in avail")
-        seen[pattern] = True
-        return True
+        possible = True
     i = len(pattern)-1 #last index
     while i > 0:
         possible = recurse(avail, pattern[:i]) and recurse(avail, pattern[i:])
         if possible:
+            arrange.append(pattern[:i]
             break
         i -= 1
     seen[pattern] = possible
