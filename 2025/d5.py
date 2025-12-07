1cba3e0e2fa60d3a3a0155ad755ad99f0090eb0b
Hoc Vo <chohocvo@gmail.com>
2025-12-06 16:14:11 -0800

    d5 p2

--- 2025/d5.py
+++ 2025/d5.py
@@ -12,7 +12,7 @@ def main():
     count = 0
     foundBreak = False
     ingredients = []
-    freshIng = set()
+    freshIng = []
     for line in lines:
         # line = lines[i]
         if line == '':
@@ -22,7 +22,7 @@ def main():
             ingredients.append(int(line))
         else:
             low,high = line.split('-')
-            freshIng.add((int(low),int(high)))
+            freshIng.append((int(low),int(high)))
     # print(ingredients)
     # print(freshIng)
     for ing in ingredients:
@@ -30,6 +30,29 @@ def main():
             if ing >= fresh[0] and ing <= fresh[1]:
                 count += 1
                 break
+    print(count)
+    count=0
+    freshIng = sorted(freshIng)
+    print(freshIng)
+    new_fresh = []
+    new_fresh.append(freshIng[0])
+    idx = 0
+    for i in range(1,len(freshIng)):
+        l,h = new_fresh[idx]
+        l1,h1 = freshIng[i]
+        if l <= l1 <= h:
+            #inclusive
+            if h1 <= h:
+                continue
+            # longer case:
+            new_fresh[idx] = (l,l1-1)
+            new_fresh.append((l1,h1))
+        else:
+            new_fresh.append((l1,h1))
+        idx += 1
+    print(new_fresh)
+    for l,h in new_fresh:
+        count += h-l+1
     print(count)
 start = time.time()
 main()
