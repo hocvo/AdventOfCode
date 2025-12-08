ada97d51419feea3f7bac7990cd23e1d0d87df1d
Hoc Vo <hoc.vo@baesystems.us>
2025-12-08 13:32:34 -0800

    d8 p1 done

--- 2025/d8.py
+++ 2025/d8.py
@@ -1,8 +1,9 @@
 import util
 import time
 import math
+import heapq
 
-lines = util.parse('test.txt')
+lines = util.parse('d8.txt')
 
 def calc_dist(p, q):
     d = (q[0] - p[0]) ** 2 + (q[1] - p[1]) ** 2 + (q[2] - p[2]) ** 2
@@ -19,57 +20,51 @@ def main():
             dist_map[(i,j)] = dist
 
     circuits = []
+    heap = []
     sorted_dist_items = sorted(dist_map.items(), key=lambda item: item[1])
 
     connections = 0
     idx = 0
     remain = set(range(len(lines)))
-    while connections <= 10:
+    while idx < 1000:
         closest = sorted_dist_items[idx]
         # print(junc_boxes[closest[0][0]], junc_boxes[closest[0][1]])
-        found = False
-        # c = x for i,x in enumerate(circuits) if (closest[0][0] in x or closest[0][1] in x)
-        c = next((x for x in circuits if closest[0][0] in x), set())
-        prev_len = len(c)
-        if not c:
+        it = iter(circuits)
+        # c = next((x for x in circuits if closest[0][0] in x or closest[0][1] in x), set())
+        cs = [x for x in circuits if closest[0][0] in x or closest[0][1] in x]
+        # prev_len = len(c)
+        if len(cs) == 0:
+            c = set()
+            c.add(closest[0][0])
+            c.add(closest[0][1])
             circuits.append(c)
-        c.add(closest[0][0])
-        c.add(closest[0][1])
-        if len(c) > prev_len:
             connections += 1
-        # for c in circuits:
-        #     if closest[0][0] in c and closest[0][1] in c:
-        #         found = True
-        #         break
-        #     elif closest[0][0] in c or closest[0][1] in c:
-        #         c.add(closest[0][0])
-        #         c.add(closest[0][1])
-        #         if closest[0][0] in remain:
-        #             remain.remove(closest[0][0])
-        #         if closest[0][1] in remain:
-        #             remain.remove(closest[0][1])
-        #         connections += 1
-        #         found = True
-        #         break
-        # if not found:
-        #     cir = set()
-        #     cir.add(closest[0][0])
-        #     cir.add(closest[0][1])
-        #     if closest[0][0] in remain:
-        #         remain.remove(closest[0][0])
-        #     if closest[0][1] in remain:
-        #         remain.remove(closest[0][1])
-        #     circuits.append(cir)
-        #     connections += 1
+        elif len(cs) == 1:
+            c = cs[0]
+            # print('adding',closest[0][0],closest[0][1], 'to set',c)
+            c.add(closest[0][0])
+            c.add(closest[0][1])
+            connections += 1
+        else:
+            # print('found',closest[0][0],closest[0][1], 'in 2 different set',cs)
+            cs[0].update(cs[1])
+            circuits.remove(cs[1])
+
+        # remain.discard(closest[0][0])
+        # remain.discard(closest[0][1])
+        # if len(c) > prev_len:
+            # connections += 1
 
         # print('remain:',len(remain))
         idx += 1
-    # print('circuits:', len(circuits))
-    # print(circuits)
-    circuits.sort(key=len,reverse=True)
+    for c in circuits:  
+        # print(c)
+        heapq.heappush(heap, len(c))
+    while len(heap) > 3:
+        heapq.heappop(heap)
     cir_size = 1
-    for i in range(3):
-        cir_size *= len(circuits[i])
+    while heap:
+        cir_size *= heapq.heappop(heap)
     print(cir_size)
 
 start = time.time()
@@ -78,3 +73,8 @@ stop = time.time()
 print('Finish in:',(stop-start)/1000,'s')
 
 #p1 7106 too low
+#p1 9240 not right
+#8779, 8360 not right
+#using heapq 9702 not right
+#13800 not right
+#ans = 122430
