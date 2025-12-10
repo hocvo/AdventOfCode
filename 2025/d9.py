653e7db6f660c2d9c28890399d10a350d2793de9
Hoc Vo <hoc.vo@baesystems.us>
2025-12-09 17:04:20 -0800

    d9 ray cast doesnot work

--- 2025/d9.py
+++ 2025/d9.py
@@ -1,7 +1,7 @@
 import util
 import time
-import matplotlib.pyplot as plt
-import numpy as np
+# import matplotlib.pyplot as plt
+# import numpy as np
 lines = util.parse('d9.txt')
 
 def area(xy, i, j):
@@ -21,21 +21,21 @@ def part1():
             max_area = max(max_area, area(xy,i,j))
     print(max_area)
 
-def is_covered(testx, testy,xy, max_x, max_y):
-    return True
+def is_covered(testx, testy,xy):
+    return util.pointInPolygon2D(testx, testy,xy)
         
-def check_green(xy, x1,y1,x2,y2,max_x, max_y):
+def check_green(xy, x1,y1,x2,y2):
     if x1 == x2 or y1 == y2:
         return True
     # points to find
     x,y = x1,y2
     xx,yy = x2,y1
-    return is_covered(x,y,xy,max_x,max_y) & is_covered(xx,yy,xy,max_x,max_y)
+    return is_covered(x,y,xy) & is_covered(xx,yy,xy)
 
-def area2(xy, i, j, max_x, max_y):
+def area2(xy, i, j):
     x1,y1 = xy[i]
     x2,y2 = xy[j]
-    if check_green(xy,x1,y1,x2,y2, max_x,max_y):
+    if check_green(xy,x1,y1,x2,y2):
         return (abs(x1-x2)+1) * (abs(y1-y2)+1)
     return 0
 
@@ -47,15 +47,15 @@ def part2():
         x,y = int(x),int(y)
         xy.append((x,y))
 
-    max_x = max(xy, key=lambda x: x[0])
-    max_y = max(xy, key=lambda x: x[1])
-    fig, ax = plt.subplots()
-    ax.scatter([x[0] for x in xy], [x[1] for x in xy], color='blue', label='Initial Points')
-    plt.show()
+    # max_x = max(xy, key=lambda x: x[0])
+    # max_y = max(xy, key=lambda x: x[1])
+    # fig, ax = plt.subplots()
+    # ax.scatter([x[0] for x in xy], [x[1] for x in xy], color='blue', label='Initial Points')
+    # plt.show()
     prev_max = max_area
     for i in range(len(xy)):
         for j in range(i+1,len(xy)):
-            test_area = area2(xy,i,j,max_x,max_y)
+            test_area = area2(xy,i,j)
             max_area = max(max_area, test_area)
             # print('area:', xy[i],xy[j], test_area)
             if max_area > prev_max:
--- 2025/util.py
+++ 2025/util.py
@@ -176,8 +176,81 @@ def find(matrix, value):
                 return (r,c)
     return (-1,-1)
 
-def pointInPolygon2D(xy,x,y, max_x,max_y):
+#https://www.geeksforgeeks.org/dsa/check-if-two-given-line-segments-intersect/
+# function to check if point q lies on line segment 'pr'
+def onSegment(p, q, r):
+    return (q[0] <= max(p[0], r[0]) and q[0] >= min(p[0], r[0]) and
+            q[1] <= max(p[1], r[1]) and q[1] >= min(p[1], r[1]))
 
+# function to find orientation of ordered triplet (p, q, r)
+# 0 --> p, q and r are collinear
+# 1 --> Clockwise
+# 2 --> Counterclockwise
+def orientation(p, q, r):
+    val = (q[1] - p[1]) * (r[0] - q[0]) - \
+          (q[0] - p[0]) * (r[1] - q[1])
+
+    # collinear
+    if val == 0:
+        return 0
+
+    # clock or counterclock wise
+    # 1 for clockwise, 2 for counterclockwise
+    return 1 if val > 0 else 2
+
+
+# function to check if two line segments intersect
+def doIntersect(seg1,seg2):
+    # find the four orientations needed
+    # for general and special cases
+    o1 = orientation(seg1[0], seg1[1], seg2[0])
+    o2 = orientation(seg1[0], seg1[1], seg2[1])
+    o3 = orientation(seg2[0], seg2[1], seg1[0])
+    o4 = orientation(seg2[0], seg2[1], seg1[1])
+
+    # general case
+    if o1 != o2 and o3 != o4:
+        return True
+
+    # special cases
+    # p1, q1 and p2 are collinear and p2 lies on segment p1q1
+    if o1 == 0 and onSegment(seg1[0], seg2[0], seg1[1]):
+        return True
+
+    # p1, q1 and q2 are collinear and q2 lies on segment p1q1
+    if o2 == 0 and onSegment(seg1[0], seg2[1], seg1[1]):
+        return True
+
+    # p2, q2 and p1 are collinear and p1 lies on segment p2q2
+    if o3 == 0 and onSegment(seg2[0], seg1[0], seg2[1]):
+        return True
+
+    # p2, q2 and q1 are collinear and q1 lies on segment p2q2 
+    if o4 == 0 and onSegment(seg2[0], seg1[1], seg2[1]):
+        return True
+
+    return False
+    
+# assume 0,0 is at top left. +x going right
+# +y is going down
+def pointInPolygon2D(x,y,xy):
+    #test horizon:
+    segment = [(0,y),(x,y)]
+    horizontal_count = 0
+    for i in range(len(xy)):
+        polygon_segment = [xy[i],xy[(i+1)%len(xy)]]
+        horizontal_count += 1 if doIntersect(segment, polygon_segment) else 0
+    
+    #test vertical:
+    segment = [(x,0),(x,y)]
+    vertical_count = 0
+    # for i in range(len(xy)):
+        # polygon_segment = [xy[i],xy[(i+1)%len(xy)]]
+        # vertical_count += 1 if doIntersect(segment, polygon_segment) else 0
+    if horizontal_count%2 == 1 or vertical_count%2 == 1:
+        return True
+    # print((x,y), 'is not inside')
+    return False
 # useful function
 #     xy is a list of tuple (x,y)
 #     # sorted_x = sorted(xy, key=lambda x: (x[0],x[1]))
